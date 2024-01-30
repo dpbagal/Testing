2479d527f48e485d4a0adb85adcc25631ee2b858
dbagal <bagaldnyaneshwar226@gmail.com>
2024-01-30 17:37:17 +0530

    First step is done added to local repository

---
+++ GitHub-FirstUpload/src/com/first/github/pckg/Stepone.java
@@ -0,0 +1,9 @@
+package com.first.github.pckg;
+
+public class Stepone {
+public static void main(String[] args) {
+	System.out.println("First folder created ");
+	System.out.println("Then gitbash open and git init command");
+	System.out.println("Then opened eclipse and created project saw ? on it till here");
+}
+}
